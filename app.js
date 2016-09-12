c6c5432011758fb608c7859933b56d1a6cc18d5f
detroitcoder9 <alexmazur6@gmail.com>
2016-09-12 02:02:02 -0400

    Finished Challenge

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	for(var i = 1; i < 101; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			$("body").append("<p>fizzbuzz</p>");
+		}
+		else if (i % 5 == 0) {
+			$("body").append("<p>buzz</p>");
+		}
+		else if (i % 3 == 0) {
+			$("body").append("<p>fizz</p>");
+		}
+		else
+			$("body").append("<p>" + i + "</p>");
+	}
+});
