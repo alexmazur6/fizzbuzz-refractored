4ea271819c8a34edd3c90fae3f08fd9d69586319
detroitcoder9 <alexmazur6@gmail.com>
2016-09-12 03:10:52 -0400

    Final Refractor Commit

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ $(document).ready(function() {
 	var userNumber;
 	$("form").submit(function(event) {
 		event.preventDefault();
+		$("p").remove();
 		userNumber = $("input").val();
 		userNumber = parseInt(userNumber);
-
-		for(var i = 1; i < (userNumber + 1); i++) {
-			console.log(i);
-		}
-
+		fizzbuzz(userNumber);
 		$(this)[0].reset();
 	});
+
+	function fizzbuzz( numberTo ) {
+		for (var i = 1; i < numberTo + 1; i++) {
+			if ( i % 3 == 0 && i % 5 == 0) {
+				$(".container").append("<p>fizzbuzz</p>");
+			}
+			else if ( i % 3 == 0 ) {
+				$(".container").append("<p>fizz</p>");
+			}
+			else if ( i % 5 == 0 ) {
+				$(".container").append("<p>buzz</p>");
+			}
+			else {
+				$(".container").append("<p>" + i + "</p>");
+			}
+		}
+	}
 });
